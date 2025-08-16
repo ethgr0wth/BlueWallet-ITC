891eb2125e6d0fa29df7c809ee92eacbde9397df
gr0wth.eth.xmr <38635290+ethgr0wth@users.noreply.github.com>
2025-08-16 18:54:56 -0400

    Update blockExplorer.ts

--- models/blockExplorer.ts
+++ models/blockExplorer.ts
@@ -8,9 +8,8 @@ export interface BlockExplorer {
 }
 
 export const BLOCK_EXPLORERS: { [key: string]: BlockExplorer } = {
-  default: { key: 'default', name: 'Mempool.space', url: 'https://mempool.space' },
-  blockchair: { key: 'blockchair', name: 'Blockchair', url: 'https://blockchair.com/interchained' },
-  blockstream: { key: 'blockstream', name: 'Blockstream.info', url: 'https://blockstream.info' },
+  default: { key: 'default', name: 'Interchained', url: 'https://explorer.interchained.org' },
+  interchained: { key: 'interchained', name: 'Interchained', url: 'https://explorer.interchained.com' },
   custom: { key: 'custom', name: 'Custom', url: '' }, // Custom URL will be handled separately
 };
 
