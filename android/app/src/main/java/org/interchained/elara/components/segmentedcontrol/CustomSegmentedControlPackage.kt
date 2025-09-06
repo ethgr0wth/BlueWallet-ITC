6fcca9f3cee369f6179c72bfef593e95a1949ede
gr0wth.eth.xmr <38635290+ethgr0wth@users.noreply.github.com>
2025-09-06 09:51:45 -0400

    Update CustomSegmentedControlPackage.kt

--- android/app/src/main/java/org/interchained/elara/components/segmentedcontrol/CustomSegmentedControlPackage.kt
+++ android/app/src/main/java/org/interchained/elara/components/segmentedcontrol/CustomSegmentedControlPackage.kt
@@ -1,8 +1,8 @@
 package org.interchained.elara.components.segmentedcontrol
 
-import org.interchained.elara.ReactPackage
+import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
-import org.interchained.elara.ReactApplicationContext
+import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
 
 class CustomSegmentedControlPackage : ReactPackage {
